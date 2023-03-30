1eeaf6f005174cf2b3f555462779ce70946227f0
藤井涼平 <>
2023-03-31 04:35:51 +0900

    task モデルのバリデーションのテストを実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,28 @@
 require "rails_helper"
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "title を指定している時" do
+    it "task が作られる" do
+      task = Task.new(title: "foo", completed: "false")
+      expect(task.valid?).to eq true
+    end
+  end
+
+  context "title を指定していない時" do
+    it "task の作成に失敗する" do
+      task = Task.new(title: nil, completed: "false")
+      expect(task).to be_invalid
+      expect(task.errors.details[:title][0][:error]).to eq :blank
+      # binding.pry
+    end
+  end
+
+  context "completed を指定していない時" do
+    it "デフォルトの false の値が参照される" do
+      task = Task.new(title: "foo")
+      expect(task).to be_valid
+      expect(task.completed).to eq false
+      # binding.pry
+    end
+  end
 end
