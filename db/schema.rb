0137110b7d8227947cf7ad6eec18a5704eb94d99
藤井涼平 <>
2023-03-31 01:48:16 +0900

    completed カラムのデフォルトを設定する migration を作成し反映させた。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_30_153746) do
+ActiveRecord::Schema.define(version: 2023_03_30_164453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
